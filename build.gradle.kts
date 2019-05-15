e31888a7f369a5bc692d4d99e2b94654ea42f9f8
RANGNATHAN <ranganathan@shamlatech.com>
2019-05-15 10:39:55 +0530

    classpath and plugin added

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,6 +12,7 @@ buildscript {
     dependencies {
         classpath("com.android.tools.build:gradle:${Versions.gradle_version}")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.kotlin_version}")
+        classpath ("com.github.dcendents:android-maven-gradle-plugin:2.1")
         /*  classpath("com.google.gms:google-services:4.2.0")
           classpath("io.fabric.tools:gradle:1.26.1")*/
         // NOTE: Do not place your application dependencies here; they belong
@@ -31,3 +32,5 @@ allprojects {
         maven(url = "https://maven.google.com")
     }
 }
+apply(mapOf("plugin" to "com.github.dcendents.android-maven"))
+
