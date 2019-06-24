d996c88d2d6c3b04fb55c60068301c103b000d0e
RanganathanApps <apps.ranganathan@gmail.com>
2019-06-24 16:31:18 +0530

    force update checker

--- configlibrary/src/main/java/apps/ranganathan/configlibrary/activity/UtilActivity.kt
+++ configlibrary/src/main/java/apps/ranganathan/configlibrary/activity/UtilActivity.kt
@@ -1,6 +1,9 @@
 package apps.ranganathan.configlibrary.activity
 
 import android.content.Context
+import android.content.DialogInterface
+import android.content.Intent
+import android.net.Uri
 import android.os.Build
 import android.os.VibrationEffect
 import androidx.appcompat.app.AppCompatActivity
@@ -10,13 +13,44 @@ import android.view.View
 import android.view.inputmethod.InputMethodManager
 import android.widget.Toast
 import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AlertDialog
 import apps.ranganathan.configlibrary.base.BaseAppActivity
+import apps.ranganathan.configlibrary.utils.ForceUpdateChecker
 import apps.ranganathan.configlibrary.utils.LogManager
 import apps.ranganathan.configlibrary.utils.ToastManager
 import apps.ranganathan.configlibrary.utils.VibrateManager
 import com.google.android.material.snackbar.Snackbar
 
-open class UtilActivity : AppCompatActivity(), ToastManager, LogManager, VibrateManager {
+open class UtilActivity : AppCompatActivity(), ToastManager, LogManager, VibrateManager,ForceUpdateChecker.OnUpdateNeededListener {
+
+    override fun onUpdateNeeded(updateUrl: String) {
+        makeLog("onUpdateNeeded :$updateUrl")
+        val dialog = AlertDialog.Builder(this)
+            .setTitle("New version available")
+            .setMessage("Please, update app to the newer version to continue..")
+            .setPositiveButton("Update", DialogInterface.OnClickListener { dialog, which ->
+                redirectStore(updateUrl)
+            }).setNegativeButton("cancel", null).create()
+
+        dialog.show()
+    }
+
+    override fun onUpToDate() {
+        makeLog("onUpToDate :")
+        val dialog = AlertDialog.Builder(this)
+            .setTitle("App is up to date!")
+            .setMessage("You are currently using a latest one!")
+            .setPositiveButton("Ok", DialogInterface.OnClickListener { dialog, which ->
+            }).create()
+
+        dialog.show()
+    }
+
+    private fun redirectStore(updateUrl: String) {
+        val intent = Intent(Intent.ACTION_VIEW, Uri.parse(updateUrl))
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        startActivity(intent)
+    }
 
     override val context: Context
         get() = this
