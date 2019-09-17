8f325037136eae265d33231f6dad37ea4c64880e
RanganathanApps <apps.ranganathan@gmail.com>
2019-09-17 11:44:59 +0530

    API constants

---
+++ configlibrary/src/main/java/apps/ranganathan/configlibrary/utils/APIConstants.kt
@@ -0,0 +1,11 @@
+package apps.ranganathan.configlibrary.utils
+
+open class APIConstants {
+
+    var PROTOCOL = "http://"
+    var IP = "192.168.1.100/"
+    var APP_CONTEXT = "app/"
+    var VERSION = "v1/"
+    var URL = PROTOCOL+IP+APP_CONTEXT+VERSION
+
+}
